fd043b8954bc97b855210b95e9b9a348f2ef88ac
Nikhil Bhatt <nikhil.1.bhatt@nokia.com>
2023-06-04 16:41:45 +0530

    changes

---
+++ src/EvenOrOdd.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class EvenOrOdd {
+    
+    public void check(int inputNumber){
+    
+      if(inputNumber%2==0){
+        System.out.println("Even"+inputNumber);
+      }else{
+        System.out.println("odd"+inputNumber);
+      }
+    }
+    public static void main(String[] args) throws Exception {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number you want to check : ");
+        int inputNumber=sc.nextInt();
+        EvenOrOdd e=new EvenOrOdd();
+        e.check(inputNumber);
+        
+    }
+}
