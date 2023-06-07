8238cb1ed417fd6a8dedbda871d079d181975fe2
nikhil-795 <nikhilbhatt795@gmail.com>
2023-06-07 23:34:37 +0530

    print add

---
+++ src/Day2/PrintuptoNumber.java
@@ -0,0 +1,20 @@
+package Day2;
+
+import java.util.Scanner;
+
+public class PrintuptoNumber {
+    public void check(int inputInteger){
+        for(int start=1;start<=inputInteger;start++){
+            System.out.println(start);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter Integer: ");
+        int inputInteger=sc.nextInt();
+        PrintuptoNumber c=new PrintuptoNumber();
+        c.check(inputInteger);
+        sc.close();
+    }
+    
+}
