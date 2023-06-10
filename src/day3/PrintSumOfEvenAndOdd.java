89076f1b3359447638ab065d96ba06d39df6ddb6
nikhil-795 <nikhilbhatt795@gmail.com>
2023-06-10 14:47:56 +0530

    add

--- src/day3/PrintSumOfEvenAndOdd.java
+++ src/day3/PrintSumOfEvenAndOdd.java
@@ -1,6 +1,27 @@
 package day3;
 
+import java.util.Scanner;
+
 public class PrintSumOfEvenAndOdd {
+    public static void check(int limitOfSeries){
+        int SumOfEvenDigit=0;
+        int SumOfOddDigit=0;
+        for(int start=1;start<=limitOfSeries;start++){
+            if(start%2==0){
+                SumOfEvenDigit+=start;
+            }else{
+                SumOfOddDigit+=start;
+            }
+        }
+        System.out.println("Sum of Even digits is: "+SumOfEvenDigit);
+        System.out.println("Sum of Odd digits : "+ SumOfOddDigit);
+    }
     
-    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter number :");
+        int limitOfSeries=sc.nextInt();
+        PrintSumOfEvenAndOdd.check(limitOfSeries);
+        sc.close();
+    }
 }
