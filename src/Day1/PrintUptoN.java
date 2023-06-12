8a29a73ab8bae37bbb25de3b4aeeaca7fa3863ca
nikhil-795 <nikhilbhatt795@gmail.com>
2023-06-12 16:34:36 +0530

    changes

--- src/Day1/PrintUptoN.java
+++ src/Day1/PrintUptoN.java
@@ -3,10 +3,9 @@ import java.util.Scanner;
 
 public class PrintUptoN {
     public void check(int input){
-        for(int start=1;start<=input;start++){
-            System.out.println(start+" ");
-        }
+   
     }
+    
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter number till you want to print ");
