90152c42a011db6ee4d0ab72bc72afddd49ef20d
nikhil-795 <nikhilbhatt795@gmail.com>
2023-06-12 21:27:30 +0530

    pattern3

--- src/Day4/Pattern3.java
+++ src/Day4/Pattern3.java
@@ -1,7 +1,26 @@
 package Day4;
 
+import java.util.Scanner;
+
 public class Pattern3 {
+
+    public static void print(int row, int column){
+        for(int i=1;i<=row;i++){
+            for(int j=1;j<=column;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+     
     public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      System.out.print("Enter rows : ");
+      int row=sc.nextInt();
+      System.out.print("Enter column : ");
+      int column=sc.nextInt();
+      Pattern3.print(row,column);
+      sc.close();
         
     }
     
