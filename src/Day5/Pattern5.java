75794079eb77f4f5004c7f7b78e670192b0e4f12
nikhil-795 <nikhilbhatt795@gmail.com>
2023-06-16 02:00:49 +0530

    add

--- src/Day5/Pattern5.java
+++ src/Day5/Pattern5.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class Pattern5 {
     public static void print(int row, int column){
-        
+        char c='A';
         for(int i=1;i<=row;i++){
             
-            for(int j=1;j<=i;j++){
-               System.out.print(j+" ");
+            for(int j=1;j<=column;j++){
+               System.out.print(c+++" ");
             }
             System.out.println();
         }
