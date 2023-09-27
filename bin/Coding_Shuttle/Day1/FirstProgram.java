cdc9cfe898b62ab2ee680eddaecec3ef9f0df8fa
nikhil-795 <nikhilbhatt795@gmail.com>
2023-09-27 14:18:58 +0530

    add new

---
+++ bin/Coding_Shuttle/Day1/FirstProgram.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int s=sc.nextInt();
+        System.out.println(s);
+        sc.close();
+    }
+    
+}
